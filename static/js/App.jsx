3629568cf03509ee649100af4d4a8917b5b0caa6
Melody Lin <11lin.m7@gmail.com>
2017-10-11 23:54:40 -0400

    added react component

---
+++ static/js/App.jsx
@@ -0,0 +1,8 @@
+// App.jsx
+import React from "react";
+
+export default class App extends React.Component {
+	render() {
+		return <h1>Let us help you find the best trip!</h1>;
+	}
+}
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,1 +1,7 @@
-alert("Hello World!");
+// index.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const element = <App />;
+ReactDOM.render(element, document.getElementById('content'));
